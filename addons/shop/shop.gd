3c4e6a5f35a2787b15468f2a4d456d818944766c
Chunky Skull <hector@hectordiaz.pro>
2025-09-24 22:07:11 -0600

    shop plugin setup

---
+++ addons/shop/shop.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var inventory #a linked list with item_database_id, price, and quantity
+var shopping_cart #an array or linked list of items the player has selected
+
+func buy() -> void:
+	pass
+
+func sell() -> void:
+	pass
