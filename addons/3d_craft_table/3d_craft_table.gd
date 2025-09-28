70caabd616051bae6377d7ba01845acbde511469
Chunky Skull <hector@hectordiaz.pro>
2025-09-27 20:13:03 -0600

    renamed plugins and files to keep things more consistent

--- addons/3d_craft_table/3d_craft_table.gd
+++ addons/3d_craft_table/3d_craft_table.gd
@@ -1,31 +1,24 @@
 extends Node
 
-var shopping_cart #an array or linked list of items the player has selected to buy
 var item_repo #a linked list of items the maker can make
 	# each entry needs to have:
 		# item ID
 		# item qauntity
 		# item raw material cost or reciepe
 
-# rename this plugin to "3D Item Maker"
 # only needs the ID for game play items
 # needs access to:
 	# items available to make
 	# character's inventory:
 		# CRUD operations on raw materials there in
 		# CRUD operations on items there in
+# when the character activates a crafting table, their inventory is passed to the craft table
 
-func buy() -> void:
+func activate() -> void:
+	# opens the craft item menu
+	# highlights items that the character has enough or the right raw materials to make
 	pass
 
-func sell() -> void:
-	pass
-
-func add_to_cart(item) -> void:
-	pass
-
-func remove_from_cart(item) -> void:
-	pass
-
-func empty_cart(item)-> void:
+func deactivate() -> void:
+	# closes the craft item menu
 	pass
